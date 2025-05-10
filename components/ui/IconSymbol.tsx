276fe6c827c19a2a078e0aa5da8682fc305585be
bad1987 <badinfo1987@gmail.com>
2025-05-10 20:18:48 +0100

    project init

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -16,8 +16,8 @@ type IconSymbolName = keyof typeof MAPPING;
 const MAPPING = {
   'house.fill': 'home',
   'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'person.fill': 'person',
 } as IconMapping;
 
 /**
